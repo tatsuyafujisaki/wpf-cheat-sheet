7fa970b7a175b5dc4a3a2bfc3376e3d60a03d188
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-09-27 23:47:19 +0900

    Add GetMouseScreen()

---
+++ WpfUtil/U.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WpfUtil
+{
+    static class U
+    {
+        static Screen GetMouseScreen()
+        {
+            return Screen.FromPoint(Control.MousePosition);
+        }
+    }
+}
